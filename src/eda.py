42a0d6eb40e922c18321f8f3336ab30d22707795
YusrahS <112873830+YusrahS@users.noreply.github.com>
2026-03-30 16:27:51 +0100

    Update eda.py
    added 2 more functions

--- src/eda.py
+++ src/eda.py
@@ -451,6 +451,25 @@ class EDA:
 
         return variety_length, sentiment_length, sarcasm_length
 
+    def sarcasm_extremes(per_variety):
+        # Find most and least sarcastic varieties
+        most_sarcastic = per_variety[1].idxmax()
+        least_sarcastic = per_variety[1].idxmin()
+    
+        # Get percentages
+        most_sarcastic_pct = per_variety.loc[most_sarcastic, 1]
+        least_sarcastic_pct = per_variety.loc[least_sarcastic, 1]
+    
+        return {
+            'most_sarcastic': most_sarcastic,
+            'most_sarcastic_pct': most_sarcastic_pct,
+            'least_sarcastic': least_sarcastic,
+            'least_sarcastic_pct': least_sarcastic_pct
+        }
+    
+        #to use function,
+        #sarcasm_extremes(per_variety)
+
     def plot_text_length_analysis(self, variety_length, sentiment_length, sarcasm_length, threshold, save=False):
         save_path = "./reports/figures"
         os.makedirs(save_path, exist_ok=True)
