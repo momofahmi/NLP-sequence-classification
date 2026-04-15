4423d24651399fb04ba7bee0dfd2f830f6a41d97
YusrahS <112873830+YusrahS@users.noreply.github.com>
2026-04-15 11:22:17 +0100

    Update lr_feature_extraction.py
    Added glove embeddings feature extraction

--- src/lr_feature_extraction.py
+++ src/lr_feature_extraction.py
@@ -1,4 +1,4 @@
-"""LR_Feature_Extraction
+"""LR_Feature_Extraction for TFIDF and GloVe
 Date: 07/04/2026
 """
 
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import save_npz, load_npz
 import joblib
 import os
+import numpy as np
 
 def tfidf_features(df_train, df_validation, df_test, text_column='text',
                    max_features=15000, save_path="./tfidf"):
@@ -48,3 +49,55 @@ def load_tfidf_features(save_path="./tfidf"):
     X_test = load_npz(f"{save_path}/X_test_tfidf.npz")
     
     return X_train, X_validation, X_test, vectorizer
+
+#===================== GloVe Embeddings ====================
+# Date: 15/04/2026
+
+def load_glove_embed(file_path):
+
+    glove_embeddings = {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            file_values = line.strip().split()
+            first_word = file_values[0]
+            gv_vector = np.array(file_values[1:], dtype=np.float32)
+            glove_embeddings[first_word] = gv_vector
+    return glove_embeddings
+
+
+def doc_vector(text, embeddings, embedding_dim=100):
+    doc_words = text.lower().split()
+    vectors = []
+    
+    for word in doc_words:
+        if word in embeddings:
+            vectors.append(embeddings[word])
+    
+    if len(vectors) == 0:
+        return np.zeros(embedding_dim)
+    
+    return np.mean(vectors, axis=0)
+
+
+def glove_features(df_train, df_val, df_test, text_column='clean_text',
+                          file_path='glove.6B.100d.txt',
+                          embedding_dim=100,
+                          save_path='./glove_features'):
+
+    os.makedirs(save_path, exist_ok=True)
+    glove_embeddings = load_glove_embed(file_path)
+
+    X_train = np.array([doc_vector(text, glove_embeddings, embedding_dim) 
+                        for text in df_train[text_column]])
+
+    X_validation = np.array([doc_vector(text, glove_embeddings, embedding_dim) 
+                      for text in df_val[text_column]])
+
+    X_test = np.array([doc_vector(text, glove_embeddings, embedding_dim) 
+                       for text in df_test[text_column]])
+    
+    np.save(f'{save_path}/X_train_glove.npy', X_train)
+    np.save(f'{save_path}/X_validation_glove.npy', X_validation)
+    np.save(f'{save_path}/X_test_glove.npy', X_test)
+    
+    return X_train, X_validation, X_test
